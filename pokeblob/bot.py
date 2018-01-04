494b8603b8a0999d9aa508f1430e3594143027a3
FrostLuma <frostluma@gmail.com>
2018-01-04 06:52:45 +0100

    make uvloop optional, use yaml config, add debug tools

--- pokeblob/bot.py
+++ pokeblob/bot.py
@@ -1,13 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
 import logging
 
-from discord.ext.commands import Bot
+import asyncpg
+import yaml
+from discord.ext import commands
 
 
-class PokeBlob(Bot):
-    def __init__(self, *args, db, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.db = db
+class PokeBlob(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        with open('config.yaml', 'rb') as f:
+            self.config = config = yaml.safe_load(f)
+
+        super().__init__(command_prefix=config['prefix'], *args, **kwargs)
+
+        self.db = None
         self.logger = logging.getLogger(__name__)
+
+        self.load_extension('jishaku')
 
     async def on_ready(self):
         self.logger.info('Ready! Logged in as %s (%d)', self.user, self.user.id)
+
+    async def is_owner(self, user):
+        return user.id in self.config['admin_users']
+
+    async def start(self, *args, **kwargs):
+        while True:
+            # to allow Docker to set up PostgreSQL attempt to reconnect every 2 seconds
+            try:
+                self.db = await asyncpg.create_pool(**self.config['pg_credentials'])
+            except (asyncpg.CannotConnectNowError, ConnectionRefusedError):
+                self.logger.exception('Cannot connect to PostgreSQL, stalling:')
+                await asyncio.sleep(2)
+            else:
+                break
+
+        await super().start(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        super().run(self.config['token'], *args, **kwargs)
--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import asyncpg
 import logging
-import uvloop
 import sys
 
-import config
 from pokeblob.bot import PokeBlob
 
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
 
 # set default levels
 logging.getLogger().setLevel(logging.INFO)
@@ -18,7 +21,7 @@ logging.getLogger('pokeblob').setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
 
 # log file and stdout
-handler = logging.FileHandler(filename='pokeblobs.log', encoding='utf-8', mode='a')
+handler = logging.FileHandler(filename='pokeblob.log', encoding='utf-8', mode='a')
 handler.setFormatter(formatter)
 stream = logging.StreamHandler(stream=sys.stdout)
 stream.setFormatter(formatter)
@@ -26,21 +29,6 @@ stream.setFormatter(formatter)
 logging.getLogger().addHandler(handler)
 logging.getLogger().addHandler(stream)
 
-async def main():
-    logger = logging.getLogger('run')
 
-    while True:
-        try:
-            db = await asyncpg.create_pool(**config.pg_credentials)
-        except (ConnectionRefusedError, asyncpg.CannotConnectNowError):
-            logger.exception('Cannot connect to Postgres, stalling:')
-            await asyncio.sleep(2)
-        else:
-            break
-
-    bot = PokeBlob(db=db, command_prefix='p!')
-    logger.info('Running bot.')
-    await bot.start(config.token)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+bot = PokeBlob()
+bot.run()
